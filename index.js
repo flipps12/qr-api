e92ed72ec91aece165cb39dacb80a9c42374c361
Flipps12 <paginasypruebas12@gmail.com>
2024-03-04 22:13:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const QRCode = require('qrcode'); // Asegúrate de haber instalado la biblioteca qrcode previamente
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/qr/:link', (req, res) => {
+    const textoQR = valor = req.params.link; // Cambia esto al texto que desees codificar
+    console.log(textoQR)
+    // Genera el código QR en formato SVG (puedes elegir otros formatos también)
+    QRCode.toString(textoQR, { errorCorrectionLevel: 'H', type: 'svg' }, (err, data) => {
+        if (err) {
+            console.error('Error al generar el código QR:', err);
+            res.status(500).send('Error al generar el código QR');
+        } else {
+            // Envía la respuesta al cliente con el código QR
+            res.type('svg');
+            res.send(data);
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
